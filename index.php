08ccbef1efeb066a3192aab2a32267cf5e090985
angelsidoh <angel._ruiz@hotmail.com>
2021-01-15 07:55:11 -0600

    Tarjetas de paquetes animamadas mas contenido y botones nuevos

--- index.php
+++ index.php
@@ -5,19 +5,36 @@ require 'includes/templates/header.php'
 <div class="contenedorx">
     <div class="contenedor-paquetes">
         <div class="paquete1">
-       
-
-          
-
             <button class="draw">
                 <div class="text-paquete1">
                     <div class="title">
-
+                        <div class="text"><h1>Paquete Basico</h1></div>
+                    <div class="hr">
+                        <span><i class="far fa-bookmark"></i></span>
                     </div>
+                    </div>
+                    
                     <div class="subtitle">
-
+                        <div class="text">
+                            <h3>Ideal para empresas y negocios pequeños, además también para emprendedores.</h3>
+                            <div class="hr">
+                                <span> <i class="fas fa-angle-double-down"></i> </span>
+                            </div>
+                        </div>
                     </div>
-
+                    <div class="ventaja">
+                        <div class="text">
+                            <p>1.	Diseño básico de la página Web (En software de diseño gráfico)</p>
+                            <p>2.	Programación / Maquetación en html5, css3, php y JavaScript</p>
+                            <p>3.	Dominio gratis el primer año (Segundo año se paga una cuota)</p>
+                            <p>4.	Hosting (10GB de almacenamiento en SSD para una mejor velocidad)</p>
+                            <p>5.	Correos personalizados con tu dominio (Ejemplo: info@tudominio.com)</p>
+                            <p>6.	Certificado SSL de seguridad</p>
+                            <p>7.	Registro de una cuenta en Google Analytics para que revisé los resultados de tráfico en su sitio web.</p>
+                            
+                        </div>
+                    </div>
+                    <a href="#" class="btn third">Ver</a>
                 </div>
             </button>
 
@@ -26,28 +43,73 @@ require 'includes/templates/header.php'
         </div>
         <div class="paquete2">
         <button class="center">
-                <div class="text-paquete1">
+        <div class="text-paquete1">
                     <div class="title">
-
+                        <div class="text"><h1>Paquete Negocios</h1></div>
+                    <div class="hr">
+                        <span><i class="far fa-bookmark"></i></span>
                     </div>
-                    <div class="subtitle">
-
                     </div>
                     
+                    <div class="subtitle">
+                        <div class="text">
+                            <h3>Ideal para empresas o negocios Medianos, Profesionista Independiente, con capacidades de brindar diferentes productos y servicios.</h3>
+                            <div class="hr">
+                                <span> <i class="fas fa-angle-double-down"></i> </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ventaja">
+                        <div class="text">
+                            <p>1.	Diseño básico de la página Web (En software de diseño gráfico)</p>
+                            <p>2.	Programación / Maquetación en html5, css3, php y JavaScript además de bases de datos en MySql conectadas a la página.</p>
+                            <p>3.	Capacidad de bases de datos 1 a 20 servicios o 1 a 20 productos distintos, además de una base de datos para registrar e iniciar sesión para los usuarios de tu página web, y una sección para dar seguimientos a todos los pedidos de tus clientes de la página web. (Ejemplo enlace)</p>
+                            <p>4.	Dominio gratis el primer año (Segundo año se paga una cuota)</p>
+                            <p>5.	Hosting (10GB de almacenamiento en SSD para una mejor velocidad)</p>
+                            <p>6.	Correos personalizados con tu dominio (Ejemplo: info@tudominio.com)</p>
+                            <p>7.	Certificado SSL de seguridad</p>
+                            <p>8.	Registro de una cuenta en Google Analytics para que revisé los resultados de tráfico en su sitio web.</p>
+                            
+                        </div>
+                    </div>
+                    <a href="#" class="btn third">Ver</a>
                 </div>
             </button>
             
         </div>
         <div class="paquete3">
             <button class="draw meet">
-                <div class="text-paquete1">
+            <div class="text-paquete1">
                     <div class="title">
-
+                        <div class="text"><h1>Paquete Profesional</h1></div>
+                    <div class="hr">
+                        <span><i class="far fa-bookmark"></i></span>
                     </div>
-                    <div class="subtitle">
-
                     </div>
                     
+                    <div class="subtitle">
+                        <div class="text">
+                            <h3>Ideal para empresas o negocios Medianos/Grandes, Profesionista Independiente; con capacidades de prestar servicios y abastecer la demanda de todos sus productos.</h3>
+                            <div class="hr">
+                                <span> <i class="fas fa-angle-double-down"></i> </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ventaja">
+                        <div class="text">
+                            <p>1.	Diseño básico de la página Web (En software de diseño gráfico)</p>
+                            <p>2.	Programación / Maquetación en html5, css3, php y JavaScript además de bases de datos en MySql conectadas a la página.</p>
+                            <p>3.	Capacidad de bases de datos para todos sus servicios y productos, además de una base de datos para registrar e iniciar sesión para los usuarios de tu página, y una sección para dar seguimientos a todos los pedidos de tus clientes de la página web. (Ejemplo enlace)</p>
+                            <p>4.	Dominio gratis el primer año (Segundo año se paga una cuota)</p>
+                            <p>5.	Hosting (10GB de almacenamiento en SSD para una mejor velocidad)</p>
+                            <p>6.	Correos personalizados con tu dominio (Ejemplo: info@tudominio.com)</p>
+                            <p>7.	Certificado SSL de seguridad</p>
+                            <p>8.	Registro de una cuenta en Google Analytics para que revisé los resultados de tráfico en su sitio web.</p>
+                            <p>9.	Cursos gratuitos en una sección para ti y tus empleados, sobre el uso de la plataforma.</p>
+                            
+                        </div>
+                    </div>
+                    <a href="#" class="btn third">Ver</a>
                 </div>
             </button>
         </div>
